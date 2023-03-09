ab6dffdbec26c231dad5665de30ccd364ca8e75f
Shraddha Manoj Gujar <gujarsm19.meta@coep.ac.in>
2023-03-09 20:07:31 +0530

    Added More Files And Final Model

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
-# FraudTransactionDetection
+# iNeuron Project FraudTransactionDetection
 This repository contains a machine learning project which tells a particular transaction is fraud or not. This is project under Ineuron Internship.
 ### Software And Tools Requirements
-
 1. [Github Account](https://github.com)
 2. [HerokuAccount](https://heroku.com)
 3. [VSCodeIDE](https://code.visualstudio.com/)
 4. [GitCLI](https://git-scm.com/book/en/v2/Getting-Started-The-Command-Line)
+
+## Project Description
+
+Credit card fraud detection (CCFD) is like looking for needles in a haystack. It requires finding, out of millions of daily transactions, which ones are fraudulent. Due to the ever-increasing amount of data, it is now almost impossible for a human specialist to detect meaningful patterns from transaction data. For this reason, the use of machine learning techniques is now widespread in the field of fraud detection, where information extraction from large datasets is required.
+
+## Supervised Machine Learning
+![alt text]()
+
+
+## Method Used
+
+* Import all required libraries
+* Analysing Data
+* Visualizing the null values
+* Splitting data in Training and Testing sets
+* Feature scaling
+* KNN training
+* SVM training
+* Decision Tree training
+* Random forest training
 
 Create a new environment
 
 ```
 conda create -p venv python==3.7 -y
 ```
+
+## Prediction
+![alt text](https://github.com/)
---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('F:\Fraud Transaction Detection\FraudTransactionDetection\Model.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data_1 = float(request.form.get('time'))
+    data_2 = float(request.form.get('val1'))
+    data_3 = float(request.form.get('val2'))
+    data_4 = float(request.form.get('val3'))
+    data_5 = float(request.form.get('val4'))
+    data_6 = float(request.form.get('val5'))
+    data_7 = float(request.form.get('val6'))
+    data_8 = float(request.form.get('val7'))
+    data_9 = float(request.form.get('val8'))
+    data_10 = float(request.form.get('val9'))
+    data_11 = float(request.form.get('val10'))
+    data_12 = float(request.form.get('val11'))
+    data_13= float(request.form.get('val12'))
+    data_14= float(request.form.get('val13'))
+    data_15= float(request.form.get('val14'))
+    data_16= float(request.form.get('val15'))
+    data_17= float(request.form.get('val16'))
+    data_18= float(request.form.get('val17'))
+    data_19= float(request.form.get('val18'))
+    data_20= float(request.form.get('val19'))
+    data_21= float(request.form.get('val20'))
+    data_22= float(request.form.get('val21'))
+    data_23= float(request.form.get('val22'))
+    data_24= float(request.form.get('val23'))
+    data_25 = float(request.form.get('val24'))
+    data_26= float(request.form.get('val25'))
+    data_27 = float(request.form.get('val26'))
+    data_28= float(request.form.get('val27'))
+    data_29= float(request.form.get('val28'))
+    data_30= float(request.form.get('Amount'))
+
+
+    arr = np.array([[data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9,data_10, data_11, data_12, data_13, data_14,
+                     data_15, data_16, data_17,
+                     data_18, data_19,data_20,data_21,data_22, data_23, data_24, data_25, data_26, data_27,data_28,data_29,data_30]])
+    pred = model.predict(arr)
+    return render_template('result.html', data=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
